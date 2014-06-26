979ec1b0bbd3d6538cf79304c3f1f7c37dbbe0c9
Justin McNally <justin@kohactive.com>
2014-06-26 01:09:45 -0500

    initial import

---
+++ public-addon/ember-chrome.js
@@ -0,0 +1,50 @@
+window.{{NAMESPACE}}ENV = {{ENV}};
+window.EmberENV = window.{{NAMESPACE}}ENV.EmberENV;
+window.ENV = window.{{NAMESPACE}}ENV; // Legacy support remove when all plugins are up to date.
+
+
+
+// Ensure our window is always the size of our dom.
+
+var windowSizeTimer = null;
+document.addEventListener('DOMSubtreeModified', function() {
+    var self = this;
+    if(windowSizeTimer != null) {
+      window.clearTimeout(windowSizeTimer);
+    } 
+    windowSizeTimer = window.setTimeout(function() {
+      var newHeight = jQuery(".chromeApp").height() + "px";
+      console.log(newHeight);
+      document.body.style.height = newHeight;
+      document.getElementsByTagName("html")[0].style.height = newHeight;
+      windowSizeTimer = null;
+    }, 100);
+});
+
+
+function loadScript(src, callback)
+{
+  var s,
+      r,
+      t;
+  r = false;
+  s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.src = src;
+  s.onload = s.onreadystatechange = function() {
+    //console.log( this.readyState ); //uncomment this line to see which ready states are called.
+    if ( !r && (!this.readyState || this.readyState == 'complete') )
+    {
+      r = true;
+      callback();
+    }
+  };
+  t = document.getElementsByTagName('body')[0];
+  t.appendChild(s);
+}
+
+loadScript("assets/vendor.js", function() {
+  loadScript("assets/{{APPNAME}}.js", function() {
+    window.{{NAMESPACE}} = require('{{APPNAME}}/app')['default'].create({{NAMESPACE}}ENV.APP);
+  });
+});
