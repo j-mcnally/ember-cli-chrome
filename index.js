133872dbad2cb1dc2a5028e51342b74a69f99545
Lachlan Priest <lachlanpriest@gmail.com>
2016-02-09 10:28:12 +0800

    Update to new ember-cli and JSCS everything

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ var mergeTrees = require('broccoli-merge-trees');
 var unwatchedTree = require('./lib/unwatched-tree')
 var utilities = require('./lib/utilities')
 
-
 module.exports = {
   name: 'ember-cli-chrome',
   treeFor: function (name) {
     var treePath =  path.join('node_modules', 'ember-cli-chrome', name + '-addon');
+
     if (fs.existsSync(treePath)) {
       return unwatchedTree(treePath);
     }
+
     if (name == 'public') {
       var manifestTree = utilities.manifestTree(this.app);
       var iconTree = utilities.iconTree(this.app)
@@ -27,5 +28,5 @@ module.exports = {
     utilities.disableFingerPrints(app);
     utilities.addChromeHelper(app);
     utilities.forceHashLocation(app);
-  }
+  },
 };
--- vendor-addon/ember-chrome.js
+++ vendor-addon/ember-chrome.js
@@ -2,16 +2,18 @@
 
 var windowSizeTimer = null;
 document.addEventListener('DOMSubtreeModified', function() {
-    var self = this;
-    if(windowSizeTimer != null) {
-      window.clearTimeout(windowSizeTimer);
-    } 
-    windowSizeTimer = window.setTimeout(function() {
-      var rootElement = document.getElementsByClassName("chromeApp")[0];
+  var self = this;
+
+  if (windowSizeTimer != null) {
+    window.clearTimeout(windowSizeTimer);
+  }
+  windowSizeTimer = window.setTimeout(function() {
+      var rootElement = document.getElementsByClassName('chromeApp')[0];
+
       if (rootElement != null) {
         var newHeight = rootElement.offsetHeight;
         document.body.style.height = newHeight;
-        document.getElementsByTagName("html")[0].style.height = newHeight;
+        document.getElementsByTagName('html')[0].style.height = newHeight;
       }
       windowSizeTimer = null;
     }, 100);
